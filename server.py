1cb96622fe9d308ec4dc73071f30192e90f169e2
Haro Truong <77948805+BeforeTheStorm202@users.noreply.github.com>
2022-12-30 20:00:34 +0700

    Update README.md

--- server.py
+++ server.py
@@ -1,12 +1,7 @@
 import flwr as fl
 from flwr.common import Metrics
 from typing import List, Tuple
-"""
-fl.server.start_server(
-    server_address="127.0.0.1:8080",
-    config=fl.server.ServerConfig(num_rounds=3)
-)
-"""
+
 def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
     # Multiply accuracy of each client by number of examples used
     accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
@@ -18,9 +13,9 @@ def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
 # Create FedAvg strategy
 strategy = fl.server.strategy.FedAvg(
         fraction_fit=1.0, # Sample 100% of available clients for training
-        fraction_evaluate=0.5, # Sample 50% of available clients for evaluation
-        min_fit_clients=4,  # Never sample less than 2 clients for training
-        min_evaluate_clients=2, # Never sample less than 1 clients for evaluation
+        fraction_evaluate=1.0, # Sample 100% of available clients for evaluation
+        min_fit_clients=4,  # Never sample less than 4 clients for training
+        min_evaluate_clients=4, # Never sample less than 4 clients for evaluation
         min_available_clients=4,  # Wait until all clients are available
         evaluate_metrics_aggregation_fn=weighted_average,  # <-- pass the metric aggregation function
 )
